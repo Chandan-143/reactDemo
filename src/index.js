722c663e9a88ae5bf10abe39cb2eec92b1736dc7
chandan kumar <chandankumarmech18@gmail.com>
2023-05-20 04:20:39 +0530

    commit 4

--- src/index.js
+++ src/index.js
@@ -1,17 +1,78 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
+import ReactDOM from 'react-dom/client';
+import React, { Component } from 'react'
+
+
+
+// you can define a react compoent using 2 technique
+
+// 1 functional compoent
+
+function A(props){ // pass the argumnet in props 
+
+  let name='KUMAR' // deta is avialbale withing compoent 
+  let surname2='KUMARI'
+  // every function return something 
+  return <h1>A-- {props.suruname} {name}--- <B  suruname={surname2}/></h1>
+}
+
+
+// 2 calss compoent
+
+// class Child extend parent{}
+
+export default class B extends Component {
+
+
+
+  // 1. properties/ variable
+
+  name ='Riya' 
+   
+  // address=''; //   
+
+  // 2. Constructor
+
+  constructor(){
+    super()
+    this.address='Bihar'
+  }
+
+
+  // 3. methods
+  
+  render() {
+    return (
+      <>
+      {
+      // in order to acces any member of a class you have to use this.member
+      // this.member
+      }
+      <span>B-- {this.name} {this.props.suruname} <C /></span>
+      
+      </>
+      )
+    }
+  }
+
+  
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+let sn='CHANDAN'
+
+let C =(props)=>{
+  let name='Kumari'
+
+  return <span> C {props.children} {name}</span>
+}
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+ <>
+    <A suruname={sn}/>
+
+ 
+ 
+ </>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
